2e4398e729d3d8640541cc0128c24aa6369bfabf
msolution-kelson <kelsonfok@gmail.com>
2023-08-15 20:51:19 +0800

    update

--- hkScoutShop/src/script/index.js
+++ hkScoutShop/src/script/index.js
@@ -3,7 +3,7 @@ $(function () {
     $(".banner_slider").slick({
         dots: true,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 8000,
         centerMode: true,
         // centerPadding: "60px",
         slidesToShow: 1,
