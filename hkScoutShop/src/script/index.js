a3a74edabd36346b86bc6a13579fd74315d6e8e6
msolution-kelson <kelsonfok@gmail.com>
2023-08-15 19:27:41 +0800

    update

--- hkScoutShop/src/script/index.js
+++ hkScoutShop/src/script/index.js
@@ -1,27 +1,27 @@
 $(function () {
     console.log("hello world!!!");
-    // $(".hero-slider").slick({
-    //     dots: true,
-    //     autoplay: true,
-    //     autoplaySpeed: 10000,
-    //     // centerMode: true,
-    //     // centerPadding: "60px",
-    //     slidesToShow: 1,
-    //     // responsive: [
-    //     //     {
-    //     //         breakpoint: 600,
-    //     //         settings: {
-    //     //             slidesToShow: 2,
-    //     //             slidesToScroll: 2
-    //     //         }
-    //     //     },
-    //     //     {
-    //     //         breakpoint: 480,
-    //     //         settings: {
-    //     //             slidesToShow: 1,
-    //     //             slidesToScroll: 1
-    //     //         }
-    //     //     }
-    //     // ]
-    // });
+    $(".banner_slider").slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        centerMode: true,
+        // centerPadding: "60px",
+        slidesToShow: 1,
+        // responsive: [
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
 });
