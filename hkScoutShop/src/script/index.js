13bbbf0ae729400fd5206b4dc0e32407db645973
msolution-kelson <kelsonfok@gmail.com>
2023-08-15 18:04:13 +0800

    update

--- hkScoutShop/src/script/index.js
+++ hkScoutShop/src/script/index.js
@@ -1,6 +1,24 @@
-$(function() {
+$(function () {
     console.log("hello world!!!");
-    $("hero-slider").slick({
-
+    $(".hero-slider").slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
